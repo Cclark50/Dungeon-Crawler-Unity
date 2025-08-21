987722bf21770157985bb8075d9b680c9f0fc384
CClark50 <Christian.Clark5@snhu.edu>
2025-08-20 23:35:53 -0500

    added IsWithinBounds

--- Assets/Scripts/MovementValidator.cs
+++ Assets/Scripts/MovementValidator.cs
@@ -6,6 +6,9 @@ public class MovementValidator : MonoBehaviour
     private GridMovement _gridMovement;
     public DirectionResolver _directionResolver;
     
+    public Vector2Int _minBounds = new Vector2Int(-10, -10);
+    public Vector2Int _maxBounds = new Vector2Int(10, 10);
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,11 +28,16 @@ public class MovementValidator : MonoBehaviour
         _gridMovement.OnMovementRequested -= HandleMovementRequest;
     }
 
+    private bool IsWithinBounds(Vector2Int targetVec)
+    {
+        return ((targetVec.x <= _maxBounds.x && targetVec.x >= _minBounds.x) && (targetVec.y <= _maxBounds.y || targetVec.y >= _minBounds.y));
+    }
+
     private void HandleMovementRequest(MovementRequestedEventArgs e)
     {
         Vector2Int requestedVec = _directionResolver.GetMovementVector(e._movementIntent.fromDirection, e._movementIntent.movementType);
         Vector2Int targetVec= e._movementIntent.fromPosition + requestedVec;
-        if ((targetVec.x <= 10 && targetVec.x >= -10) && (targetVec.y <= 10 || targetVec.y >= -10))
+        if (IsWithinBounds(targetVec))
         {
             e._GridMovement.ExecuteMovement(e._movementIntent, targetVec);
         }
